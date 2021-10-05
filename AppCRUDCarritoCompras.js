0f2913d8c5a2fd49f416df417d1334c7b09f3d58
Heiver_Vivas <heivervivas@gmail.com>
2021-10-05 00:22:46 -0500

    Cambio, logo, diseño

--- AppCRUDCarritoCompras.js
+++ AppCRUDCarritoCompras.js
@@ -1,35 +1,30 @@
-
 var AppCRUDCarritoCompras = new Vue({
-    el: '#app',
-    data: {
-        mensaje: 'Elije tus productos',
-        tareas : [],
-        id: 0,
-        nuevaTarea : ''
+  el: "#app",
+  data: {
+    mensaje: "Elije tus productos",
+    tareas: [],
+    id: 0,
+    nuevaTarea: "",
+  },
+  methods: {
+    agregarTarea() {
+      //console.log(this.nuevaTarea)
+      this.tareas.push({
+        id: this.id,
+        nombre: this.nuevaTarea,
+        estado: "Esperando...",
+      });
+      this.id++;
+      this.nuevaTarea = "";
+      console.log(this.tareas);
     },
-    methods: {
-        agregarTarea(){
-            //console.log(this.nuevaTarea)
-            this.tareas.push({
-                id : this.id,
-                nombre : this.nuevaTarea,
-                estado : "Esperando...", 
-                
-            })
-            this.id++;
-            this.nuevaTarea = '';
-            console.log(this.tareas)
-        },
-        editarTarea(index){
-            //console.log('editando..'+ index)
-            this.tareas[index].estado = "(Confirmado)";
-        },
-        eliminarTarea : function(index){
-            console.log("entra a eliminar");
-            this.tareas.splice(index, 1)
-
-        }
-
-    } 
-
-})
+    editarTarea(index) {
+      //console.log('editando..'+ index)
+      this.tareas[index].estado = "(Confirmado)";
+    },
+    eliminarTarea: function (index) {
+      console.log("entra a eliminar");
+      this.tareas.splice(index, 1);
+    },
+  },
+});
